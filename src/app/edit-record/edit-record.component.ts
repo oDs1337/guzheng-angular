82a60e470196eea027ae41e565cb8135ef087d14
oDs1337 <odstobiaszcod@gmail.com>
2022-10-24 16:40:47 +0200

    add fetch post from store via id from url

--- src/app/edit-record/edit-record.component.ts
+++ src/app/edit-record/edit-record.component.ts
@@ -1,3 +1,5 @@
+import { Post } from '../post';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,12 +10,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditRecordComponent implements OnInit {
 
-  test: any = '';
+  id: any;
+  post?: Post;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private store: Store<{ posts: Post[]}>) { }
 
   ngOnInit(): void {
-    this.test = this.route.snapshot.paramMap.get('id');
+    this.getUrlId();
+    this.getPost();
+  }
+
+  getUrlId(): void{
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
+
+  getPost(): void{
+    this.store.select((state) => state.posts).subscribe((res) => {
+      res.forEach((post) => {
+        if(post.id === this.id){
+          this.post = post;
+        }
+      })
+    })
   }
 
 }
