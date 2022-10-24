e2b16f941358b743e242850c574929e7b1f91401
oDs1337 <odstobiaszcod@gmail.com>
2022-10-24 14:43:39 +0200

    add create post that send new record to mongo db

--- src/app/service/blog-api.service.ts
+++ src/app/service/blog-api.service.ts
@@ -12,7 +12,7 @@ export class BlogApiService {
 
   #posts: Subject<Post[]> = new Subject();
   #updateUrl = "http://[::1]:3000/blog-posts?filter=%7B%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%22string%22%2C%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22id%22%3A%20true%2C%0A%20%20%20%20%22author%22%3A%20true%2C%0A%20%20%20%20%22title%22%3A%20true%2C%0A%20%20%20%20%22content%22%3A%20true%2C%0A%20%20%20%20%22creationDate%22%3A%20true%2C%0A%20%20%20%20%22imageUrlLarge%22%3A%20true%2C%0A%20%20%20%20%22imageUrlSmall%22%3A%20true%2C%0A%20%20%20%20%22youtubeUrl%22%3A%20true%2C%0A%20%20%20%20%22upVotes%22%3A%20true%2C%0A%20%20%20%20%22downVotes%22%3A%20true%0A%20%20%7D%0A%7D";
-  #apiUrl = "http://[::1]:3000/blog-posts/";
+  #apiUrl = "http://[::1]:3000/blog-posts";
   constructor(private http: HttpClient, private store: Store) { }
 
   fetchPosts(): void{
@@ -29,6 +29,24 @@ export class BlogApiService {
     this.http.delete(`${this.#apiUrl}${id}`).subscribe((res) => {
       console.log(res);
     });
+  }
+
+  createPost(payload: Post){
+    // this.http.post<Post>(this.#apiUrl, {
+    //   author: payload.author,
+    //   title: payload.title,
+    //   content: payload.content,
+    //   creationDate: payload.creationDate,
+    //   imageUrlLarge: payload.imageUrlLarge,
+    //   youtubeUrl: payload.youtubeUrl,
+    //   upVotes: 0,
+    //   downVotes: 0,
+    // }).subscribe((res) => {
+    //   console.log(res);
+    // });
+    this.http.post<Post>(this.#apiUrl, payload).subscribe((res) => {
+      console.log(res);
+    })
   }
 
   getPosts(){
