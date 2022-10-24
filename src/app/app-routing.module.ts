b7a6f791bd91a8e1123585d90194b9cd83326c7c
oDs1337 <odstobiaszcod@gmail.com>
2022-10-24 16:24:55 +0200

    add routing to edit with id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
+import { EditRecordComponent } from './edit-record/edit-record.component';
 import { AddRecordComponent } from './add-record/add-record.component';
 import { BlogComponent } from './blog/blog.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'blog', pathMatch: 'full'},
-  { path: 'blog', component: BlogComponent},
-  { path: 'add-record', component: AddRecordComponent},
+  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'add-record', component: AddRecordComponent },
+  { path: 'post/:id/edit', component: EditRecordComponent },
 ];
 
 @NgModule({
