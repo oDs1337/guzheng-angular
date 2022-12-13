081f58ed5a9a9bbca79b5534767acb455b82d783
oDs1337 <odstobiaszcod@gmail.com>
2022-12-13 12:57:23 +0100

    add pipe to return parsed from timestamp date

---
+++ src/app/shared/pipes/date-transformation.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateTransformationPipe } from './date-transformation.pipe';
+
+describe('DateTransformationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateTransformationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/pipes/date-transformation.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateTransformation'
+})
+export class DateTransformationPipe implements PipeTransform {
+
+  transform(value: string, ...args: string[]): string {
+    let dateFormat = new Date(parseInt(value));
+    return `${dateFormat.getDate()}.${dateFormat.getMonth()+1}.${dateFormat.getFullYear()}`
+  }
+
+}
